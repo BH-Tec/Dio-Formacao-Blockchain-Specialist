72e405a7fc895b1d30678db5c57b6aa9b205139b
Bruno Dorea <brunodorea@outlook.com.br>
2022-10-19 10:54:16 -0300

    Coxinha de Bueno

--- DesafioIntermediarioII2/main.js
+++ DesafioIntermediarioII2/main.js
@@ -12,10 +12,10 @@
 // conseguem devorar.
 // Ah, lembre que, em troca da sua ajuda, você poderá comer quantas coxinhas conseguir.
 
-let line = gets().split(' ');
-let H = parseInt(line[0]);
-let P = parseInt(line[1]);
+let line = gets().split(' ')
+let H = parseInt(line[0])
+let P = parseInt(line[1])
 
-let R = (H / P).toFixed(2);
+let R = (H / P).toFixed(2)
 
-console.log(R);
+console.log(R)
