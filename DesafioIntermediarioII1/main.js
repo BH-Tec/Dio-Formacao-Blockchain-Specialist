a92b0d16f7224ce1f41dfacfe6488bc654e90b6b
Bruno Dorea <brunodorea@outlook.com.br>
2022-10-19 10:53:53 -0300

    Idade em Dias

--- DesafioIntermediarioII1/main.js
+++ DesafioIntermediarioII1/main.js
@@ -4,11 +4,11 @@
 // Obs.: apenas para facilitar o cálculo, considere todo ano com 365 dias e todo mês com 30 dias.
 // Nos casos de teste nunca haverá uma situação que permite 12 meses e alguns dias, como 360, 363 ou 364.
 
-let totalDeDias = parseInt(gets());
-let meses = parseInt((totalDeDias%365) / 30);
-let anos = parseInt(totalDeDias / 365);
-let dias = parseInt((totalDeDias%365)%30);
+let totalDeDias = parseInt(gets())
+let meses = parseInt((totalDeDias%365) / 30)
+let anos = parseInt(totalDeDias / 365)
+let dias = parseInt((totalDeDias%365)%30)
 
-console.log(anos + " ano(s)");
-console.log(meses + " mes(es)");
-console.log(dias + " dia(s)");
+console.log(anos + " ano(s)")
+console.log(meses + " mes(es)")
+console.log(dias + " dia(s)")
